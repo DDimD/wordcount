db8d72516246098d2e1cda1200f0d9fef8149699
Дмитрий Дорожкин <d.dorozhkin@city-mobil.ru>
2022-04-03 18:59:41 +0300

    change stdin to os args

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package wordcount
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanWords)
-	var inputWords []string
-	for scanner.Scan() {
-		inputWords = append(inputWords, scanner.Text())
-	}
+	inputWords := os.Args[1]
 
 	if len(inputWords) <= 0 {
 		fmt.Println(0)
 		return
 	}
 
-	fmt.Println(len(inputWords))
+	wordSlice := strings.Split(inputWords, " ")
+
+	fmt.Println(len(wordSlice))
 	return
 }
